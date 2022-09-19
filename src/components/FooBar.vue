1f531ad392377669378f7648cee7c4b9058e83bd
Billhardt <patrick.billhardt@ubgnet.de>
2022-09-19 15:15:51 +0200

    initial

---
+++ src/components/FooBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="FooBar">
+    Countdown: {{ count }}
+    <button @click="reset">Reset Counter</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooBar',
+  computed: {
+    count() {
+      return this.$store.state.count;
+    }
+  },
+  methods: {
+    reset() {
+      this.$store.commit('reset');
+    }
+  }
+}
+</script>
+
+<style>
+.FooBar {
+  padding: 1rem;
+  background-color: yellowgreen;
+  margin: 1rem 0;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,32 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createStore } from 'vuex';
 
-createApp(App).mount('#app')
+import HelloWorld from './components/HelloWorld.vue';
+import FooBar from './components/FooBar.vue';
+
+import App from './App.vue';
+
+const store = createStore({
+    state() {
+        return {
+            count: 0
+        }
+    },
+    mutations: {
+        increment(state) {
+            state.count++
+        },
+        
+        reset(state) {
+            state.count = 0;
+        }
+    }
+});
+
+const app = createApp(App);
+
+app.component('HelloWorld', HelloWorld);
+app.component('FooBar', FooBar);
+
+app.use(store);
+app.mount('#app')
